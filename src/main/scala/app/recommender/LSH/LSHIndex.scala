d8624d25c3b329ae102788c2b8612cb52e0ae75e
DanThePol <daniel@thepolkadots.net>
2025-05-15 14:09:46 +0200

    chore: implement LSHIndex and NNLookup

--- src/main/scala/app/recommender/LSH/LSHIndex.scala
+++ src/main/scala/app/recommender/LSH/LSHIndex.scala
@@ -14,6 +14,18 @@ import scala.reflect.ClassTag
  */
 class LSHIndex(data: RDD[(Int, String, List[String])], seed : IndexedSeq[Int]) extends Serializable {
   private val minhash = new MinHash(seed)
+  private val state: RDD[(IndexedSeq[Int], List[(Int, String, List[String])])] = {
+    // data: (mvID, mvName, genres) -> RDD[(bucket, List[(mvID, mvName, genres)])]
+
+    val bucketedData = data.map { case (mvID, mvName, genres) => (minhash.hash(genres), (mvID, mvName, genres)) }
+    val buckets = bucketedData.aggregateByKey(List.empty[(Int, String, List[String])])(
+      { (acc, v) => v :: acc }, { (ls1, ls2) => ls1 ::: ls2 })
+
+    // Step 1: (bucket, (mvID, mvName, genres)
+    data.partitioner match
+      case Some(partitioner) => buckets.partitionBy(partitioner)
+      case None => buckets
+  }
 
   /**
    * Hash function for an RDD of queries.
@@ -21,7 +33,7 @@ class LSHIndex(data: RDD[(Int, String, List[String])], seed : IndexedSeq[Int]) e
    * @param input The RDD of keyword lists
    * @return The RDD of (signature, keyword list) pairs
    */
-  def hash(input: RDD[List[String]]) : RDD[(IndexedSeq[Int], List[String])] = {
+  def hash(input: RDD[List[String]]): RDD[(IndexedSeq[Int], List[String])] = {
     input.map(x => (minhash.hash(x), x))
   }
 
@@ -31,7 +43,8 @@ class LSHIndex(data: RDD[(Int, String, List[String])], seed : IndexedSeq[Int]) e
    * @return Data structure of LSH index
    */
   def getBuckets()
-  : RDD[(IndexedSeq[Int], List[(Int, String, List[String])])] = ???
+  : RDD[(IndexedSeq[Int], List[(Int, String, List[String])])] = state
+
 
   /**
    * Lookup operation on the LSH index
@@ -42,5 +55,12 @@ class LSHIndex(data: RDD[(Int, String, List[String])], seed : IndexedSeq[Int]) e
    *         If no match exists in the LSH index, return an empty result list.
    */
   def lookup[T: ClassTag](queries: RDD[(IndexedSeq[Int], T)])
-  : RDD[(IndexedSeq[Int], T, List[(Int, String, List[String])])] = ???
+  : RDD[(IndexedSeq[Int], T, List[(Int, String, List[String])])] = {
+
+    // queries is [signature, payload]
+    queries.leftOuterJoin(state).mapValues {
+      case (pl, Some(bucket)) => (pl, bucket)
+      case (pl, None) => (pl, List.empty[(Int, String, List[String])])
+    }.map { case (sig, (pl, bucket)) => (sig, pl, bucket) }
+  }
 }
--- src/main/scala/app/recommender/LSH/NNLookup.scala
+++ src/main/scala/app/recommender/LSH/NNLookup.scala
@@ -13,8 +13,17 @@ class NNLookup(lshIndex: LSHIndex) extends Serializable {
    * Lookup operation for queries
    *
    * @param input The RDD of keyword lists
-   * @return The RDD of (keyword list, resut) pairs
+   * @return The RDD of (keyword list, result) pairs
    */
   def lookup(queries: RDD[List[String]])
-  : RDD[(List[String], List[(Int, String, List[String])])] = ???
+  : RDD[(List[String], List[(Int, String, List[String])])] = {
+
+    val buckets = lshIndex.getBuckets()
+    val bucketedQueries = lshIndex.hash(queries)
+
+    bucketedQueries.leftOuterJoin(buckets).mapValues{
+      case (query, Some(ls)) => (query, ls)
+      case (query, None) => (query, List.empty[(Int, String, List[String])])
+    }.map{ case(id, ls) => ls }
+  }
 }
